f3ea89df3221227f3f58e697be4f85ce634e5182
LeeJungHoon <gns14585@naver.com>
2023-11-24 22:13:34 +0900

    intital commit

---
+++ src/main/java/com/example/pracprj3backend/PracPrj3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pracprj3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracPrj3BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracPrj3BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pracprj3backend/PracPrj3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pracprj3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracPrj3BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
